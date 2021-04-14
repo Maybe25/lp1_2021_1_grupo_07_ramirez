f0d6e7ae1d1a8b79f41baa47fee43cc608143328
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2021-04-13 22:20:50 -0500

    Mi primer commit

---
+++ src/main/java/com/cibertec/Dawi20211Grupo07LunesApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dawi20211Grupo07LunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dawi20211Grupo07LunesApplication.class, args);
+	}
+
+}
