0e323b10621341030d1dfe4f296e38f341f63e43
xxgdexx <wilmergabrielnegronvelasquez@gmail.com>
2021-04-20 18:44:05 -0500

    Se Creó en el paquete Repositorio una interface ModalidadRepository (Negron)

---
+++ src/main/java/com/cibertec/repositorio/ModalidadRepository.java
@@ -0,0 +1,9 @@
+package com.cibertec.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cibertec.entidad.Cliente;
+
+public interface ModalidadRepository extends JpaRepository<ModalidadRepository, Integer>{
+	
+
+}
