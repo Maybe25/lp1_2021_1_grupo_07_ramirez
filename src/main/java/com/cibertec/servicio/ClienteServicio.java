6cc277d220d7a7887f957d7db2e4d5cc7c4585f9
xxgdexx <wilmergabrielnegronvelasquez@gmail.com>
2021-04-26 10:26:09 -0500

    SE CREA EL CLIENTE SERVICIO BY: WILMER NEGRON

---
+++ src/main/java/com/cibertec/servicio/ClienteServicio.java
@@ -0,0 +1,13 @@
+package com.cibertec.servicio;
+
+import com.cibertec.entidad.Cliente;
+
+/**
+ * 
+ * @author Wilmer Negron
+ *
+ */
+public interface ClienteServicio {
+	public abstract  Cliente insertaCliente (Cliente obj);
+
+}
